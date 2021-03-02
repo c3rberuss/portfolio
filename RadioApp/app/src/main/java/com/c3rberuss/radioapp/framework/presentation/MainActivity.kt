9e4db4bcd01bcf21da65fe02820f3d058214e8bf
c3rberuss <c3rberuss@gmail.com>
2021-03-01 22:19:45 -0600

    Update portfolio

---
+++ RadioApp/app/src/main/java/com/c3rberuss/radioapp/framework/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.c3rberuss.radioapp.framework.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.c3rberuss.radioapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
